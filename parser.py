7d8fd490dd58d4c99702891e31348d185bd758e8
nikit34 <permikov134@mail.ru>
2020-09-19 20:26:31 +0300

    all testing - all working

--- parser.py
+++ parser.py
@@ -35,17 +35,15 @@ def menu():
     print("2 show all occurrences by NAME       [values]")
     print("3 show all occurrences by CONTENT    [values]")
     print("4 start proc search direct parents   [values]")
+    print("5 exit")
     while 1:
-        # act = input('input: ')
-        act = '4'
-        if next(filter(lambda x: x not in ['1', '2', '3', '4'], act), False):
+        act = input('input: ')
+        if next(filter(lambda x: x not in ['1', '2', '3', '4', '5'], act), False):
             print("Invalid input")
         else:
             print(f"{act} - OK")
             yield act
 
-
-# def gun_by_time():
 
 @open_close_file(fin=('kennedy.ged', 'r'))
 def show_by_time(file_lines, *args, **kwargs):
@@ -78,19 +76,19 @@ def show_by_time(file_lines, *args, **kwargs):
                     c += 1
                     for j, tmp_line in enumerate(file_lines):
                         if j >= i - 10 and j < i:
-                            tmp_lines.update(tmp_line.replace('//', '').replace('/-/', '').strip())
+                            tmp_lines.append(tmp_line.replace('//', '').replace('/-/', '').strip())
                         elif j == i:
-                            tmp_lines.update(tmp_line.replace('//', '').replace('/-/', '').strip())
+                            tmp_lines.append(tmp_line.replace('//', '').replace('/-/', '').strip())
                             if j > 10:
                                 k = 10
                             else:
                                 k = j
                             while k >= 0 and tmp_line.split(' ', 2)[0] != '0':
-                                select_chan.update(tmp_lines[k].replace('//', '').replace('/-/', '').strip())
+                                select_chan.append(tmp_lines[k].replace('//', '').replace('/-/', '').strip())
                                 k -= 1
                         elif j > i:
                             if tmp_line.split(' ', 2)[0] != '0' and j <= i + 10:
-                                select_chan.update(tmp_line.replace('//', '').replace('/-/', '').strip())
+                                select_chan.append(tmp_line.replace('//', '').replace('/-/', '').strip())
                             else:
                                 yield select_chan
                                 break
@@ -110,8 +108,8 @@ def show_contains_name(file_lines, *args, **kwargs):
             print(line)
     print("-------------------------------------------------------------")
 
-# TODO: w -> a+
-@open_close_file(fin=('kennedy.ged', 'r'), fout=('prolog.pl', 'w'))
+
+@open_close_file(fin=('kennedy.ged', 'r'), fout=('proc_family.pl', 'w'))
 def run_proc_prolog(file_lines, f_prolog, *args, **kwargs):
     f_prolog.write("parent(X,Y) :- parents(Y,X,_).\n")
     f_prolog.write("parent(X,Y) :- parents(Y,_,X).\n")
@@ -137,47 +135,110 @@ def run_proc_prolog(file_lines, f_prolog, *args, **kwargs):
     unit = {}
     units = []
     border = 0
+    source = {}
+    sources = []
+    full_families = []
+    ids_full_families = []
     for i, line in enumerate(file_lines, start=1):
         split_line = [x.strip() for x in line.split(' ', 2)]
         if len(split_line) == 3:
-            if split_line[2] == 'INDI' and 'id' in unit and 'name' in unit and unit['name']['i'] > border:
-                tmp = { 'id':  unit['id'] }
+            if split_line[2] == 'INDI' and 'id_unit' in unit and 'name' in unit and unit['name']['i'] > border:
+                tmp = { 'id_unit':  unit['id_unit'] }
                 tmp.update({ 'name': unit['name']['item'] })
-                for tag in ['sex', 'famc', 'fams', 'husb', 'chil', 'wife']:
+                for tag in ['sex', 'famc', 'fams',]:
                     if tag in unit and unit[tag]['i'] > border:
                         tmp.update({ tag: unit[tag]['item'] })
                 units.append(tmp)
                 border = i
                 unit = {}
             elif split_line[2] == 'INDI':
-                unit['id'] = split_line[1]
+                unit['id_unit'] = split_line[1]
             else:
-                for tag in ['NAME', 'SEX', 'FAMC', 'FAMS', 'HUSB', 'CHIL', 'WIFE']:
+                for tag in ['NAME', 'SEX', 'FAMC', 'FAMS']:
                     if split_line[1] == tag:
                         unit[tag.lower()] = { 'item': split_line[2], 'i': i }
+            if split_line[2] == 'FAM' and 'id_source' in source or split_line[1] == 'TRLR':
+                tmp = { 'id_source': source['id_source'] }
+                for tag in ['chil', 'husb', 'wife']:
+                    if tag in source and source[tag]['i'] > border:
+                        tmp.update({ tag: source[tag]['item'] })
+                sources.append(tmp)
+                border = i
+                source = {}
+            elif split_line[2] == 'FAM':
+                source['id_source'] = split_line[1]
+            else:
+                for tag in ['CHIL', 'HUSB', 'WIFE']:
+                    if split_line[1] == tag:
+                        source[tag.lower()] = { 'item': split_line[2], 'i': i }
 
-    # import pprint
-    # pprint.pprint(units)
     for unit in units:
         family = {}
         for other in units:
-            if unit['id'] != other['id']:
+            if unit['id_unit'] != other['id_unit']:
                 if 'famc' in unit and 'fams' in other and unit['famc'] == other['fams'] \
                     and 'famc' in other and other['famc'] != other['fams']:
                     if 'sex' in other:
                         if other['sex'] == 'F':
-                            family.update({'mother': other})
+                            family.update({'mother': other['name']})
                         else:
-                            family.update({'father': other})
-        if len(family) == 2:
-            child = unit['name'].lower().replace(' ', '_')
-            father = family['father']['name'].lower().replace(' ', '_')
-            mother = family['mother']['name'].lower().replace(' ', '_')
-            for warn in [';', '/', '.', ',']:
-                child = child.replace(warn, '')
-                father = father.replace(warn, '')
-                mother = mother.replace(warn, '')
-            f_prolog.write(f"parents({child}, {father}, {mother}).\n")
+                            family.update({'father': other['name']})
+            if len(family) == 2:
+                if 'famc' in unit:
+                    ids_full_families.append(unit['famc'])
+                if 'fams' in unit:
+                    ids_full_families.append(unit['fams'])
+                family.update({ 'child': unit['name']})
+                full_families.append(family)
+                break
+
+        for source in sources:
+            if unit['id_unit'] in source.values() and source['id_source'] not in ids_full_families and 'chil' in source and 'wife' in source and 'husb' in source:
+                if unit['id_unit'] == source['chil']:
+                    family.update({ 'child': unit['name'] })
+                    for source_unit in units:
+                        if source_unit['id_unit'] == source['wife']:
+                            family.update({ 'mother': source_unit['name'] })
+                        elif source_unit['id_unit'] == source['husb']:
+                            family.update({ 'father': source_unit['name'] })
+                        if len(family) == 3:
+                            ids_full_families.append(source['id_source'])
+                            full_families.append(family)
+                            break
+
+                elif unit['id_unit'] == source['wife']:
+                    family.update({ 'mother': unit['name'] })
+                    for source_unit in units:
+                        if source_unit['id_unit'] == source['chil']:
+                            family.update({ 'child': source_unit['name'] })
+                        elif source_unit['id_unit'] == source['husb']:
+                            family.update({ 'father': source_unit['name'] })
+                        if len(family) == 3:
+                            ids_full_families.append(source['id_source'])
+                            full_families.append(family)
+                            break
+
+                elif unit['id_unit'] == source['husb']:
+                    family.update({ 'father': unit['name'] })
+                    for source_unit in units:
+                        if source_unit['id_unit'] == source['chil']:
+                            family.update({ 'child': source_unit['name'] })
+                        elif source_unit['id_unit'] == source['wife']:
+                            family.update({ 'mother': source_unit['name'] })
+                        if len(family) == 3:
+                            ids_full_families.append(source['id_source'])
+                            full_families.append(family)
+                            break
+
+    for family in full_families:
+        child = family['child'].lower().replace(' ', '_')
+        mother = family['mother'].lower().replace(' ', '_')
+        father = family['father'].lower().replace(' ', '_')
+        for warn in [';', '/', '.', ',', '(', ')']:
+            child = child.replace(warn, '')
+            mother = mother.replace(warn, '')
+            father = father.replace(warn, '')
+        f_prolog.write(f"parents({child}, {father}, {mother}).\n")
 
 
 if __name__ == "__main__":
@@ -193,4 +254,4 @@ if __name__ == "__main__":
                 print("  finish")
         if act == '2' or act == '3': show_contains_name(act)
         if act == '4': run_proc_prolog()
-        break #TODO: disable
+        if act == '5': quit()
--- proc_family.pl
+++ proc_family.pl
@@ -1,33 +1,10 @@
-parents(david, george, noreen).
-parents(jennifer, george, noreen).
-parents(georgejr, george, noreen).
-parents(scott, george, noreen).
-parents(joanne, george, noreen).
-parents(jessica, david, edel).
-parents(clara, david, edel).
-parents(michael, david, edel).
-parents(laura, georgejr, susan).
-parents(anna, scott, siobhan).
-
-
-/* Relationships */
-
 parent(X,Y) :- parents(Y,X,_).
 parent(X,Y) :- parents(Y,_,X).
 
 father(X, Y) :- parents(Y, X, _).
-
-male(michael).
-male(X) :- father(X, _).
-
 mother(X, Y) :- parents(Y, _, X).
 
-female(joanne).
-female(jessica).
-female(jennifer).
-female(clara).
-female(laura).
-female(anna).
+male(X) :- father(X, _).
 female(X) :- mother(X, _).
 
 grandfather(X, Y) :- father(X, Z), father(Z, Y).
@@ -41,3 +18,29 @@ sister(X, Y) :- female(X), father(Z, X), father(Z, Y), X \= Y.
 
 aunt(X,Y) :- sister(X,Z), parent(Z,Y).
 uncle(X, Y) :- brother(X, Z), parent(Z,Y).
+
+
+
+
+
+
+% approvals
+
+
+parents(joseph_patrick_kennedy_ii, robert_francis_kennedy, ethel_skakel).
+parents(david_anthony_kennedy, robert_francis_kennedy, ethel_skakel).
+parents(michael_lemoyne_kennedy, robert_francis_kennedy, ethel_skakel).
+parents(christopher_george_kennedy, robert_francis_kennedy, ethel_skakel).
+parents(douglas_harriman_kennedy, robert_francis_kennedy, ethel_skakel).
+parents(david_lee_townsend, robert_francis_kennedy, ethel_skakel).
+parents(joseph_patrick_kennedy_iii, joseph_patrick_kennedy_ii, sheila_brewster_rauch).
+parents(lawrence_m_kane, philip_kane, bridget_murphy).
+parents(frederick_kane, lawrence_m_kane, gertrude_margaret_kane).
+parents(frederick_louis_mahoney, louis_leo_twomey, joanna_l_kennedy).
+parents(charles_joseph_burke_jr, charles_joseph_burke, margaret_louise_kennedy).
+parents(john_bernard_devine, charles_joseph_burke, margaret_louise_kennedy).
+parents(rosanna_cox, philip_cox, ellen_wilmouth).
+parents(michael_hannon, john_hannon, rosanna_cox).
+parents(william_alexander_lindsay, eustache_bouvier, therese_mercier).
+parents(louise_c_vernou, william_alexander_lindsay, elizabeth_clifford_lindsay).
+parents(william_sergeant_bud_bouvier, william_r_sergeant, maud_frances_sergeant).
